bcfd4de2a3fcdfba30341fb0bd53b9344b2b402b
Madis Ollikainen <madis_ollikainen@hotmail.com>
2015-06-04 19:05:06 +0300

    cleaning out ..

--- Ensemble/doallfour.sh
+++ Ensemble/doallfour.sh
@@ -4,9 +4,10 @@ DATE=$(date +"%Y-%m-%d")
 TIME=$(date +"%T")
 OUTDIR=out_${DATE}_${TIME}
 mkdir $OUTDIR
+PROG=ensemble_run
 
 sed "s|Choice= 1|Choice= 1|" ./dummy.conf > ./config.conf
-./inequality_inefficiency_logit_single_loop
+./$(PROG)
 mv ./time.txt $OUTDIR/time1.txt
 mv ./wealth.txt $OUTDIR/wealth1.txt
 mv ./cooperation.txt $OUTDIR/cooperation1.txt
@@ -14,21 +15,21 @@ mv ./cooperation.txt $OUTDIR/cooperation1.txt
 
 
 sed "s|Choice= 1|Choice= 2|" ./dummy.conf > ./config.conf
-./inequality_inefficiency_logit_single_loop
+./$(PROG)
 mv ./time.txt $OUTDIR/time2.txt
 mv ./wealth.txt $OUTDIR/wealth2.txt
 mv ./cooperation.txt $OUTDIR/cooperation2.txt
 
 
 sed "s|Choice= 1|Choice= 3|" ./dummy.conf > ./config.conf
-./inequality_inefficiency_logit_single_loop
+./$(PROG)
 mv ./time.txt $OUTDIR/time3.txt
 mv ./wealth.txt $OUTDIR/wealth3.txt
 mv ./cooperation.txt $OUTDIR/cooperation3.txt
 
 
 sed "s|Choice= 1|Choice= 4|" ./dummy.conf > ./config.conf
-./inequality_inefficiency_logit_single_loop
+./$(PROG)
 mv ./time.txt $OUTDIR/time4.txt
 mv ./wealth.txt $OUTDIR/wealth4.txt
 mv ./cooperation.txt $OUTDIR/cooperation4.txt
