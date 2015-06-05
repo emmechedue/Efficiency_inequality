39616c641095a8468956c1def551f6b5c6024479
Stefano Duca <emmechedue@gmail.com>
2015-06-05 15:02:21 +0200

    rechanged the dollafor.sh file

--- Single/doallfour.sh
+++ Single/doallfour.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
-DATE=$(date +"%Y-%m-%d")
-TIME=$(date +"%T")
-OUTDIR=out_${DATE}_${TIME}
-mkdir $OUTDIR
-PROG=single_run
+# DATE=$(date +"%Y-%m-%d")
+# TIME=$(date +"%T")
+# OUTDIR=out_${DATE}_${TIME}
+# mkdir $OUTDIR
 
 sed "s|Choice= 1|Choice= 1|" ./dummy.conf > ./config.conf
-./$(PROG)
-mv ./time.txt $OUTDIR/time1.txt
-mv ./wealth.txt $OUTDIR/wealth1.txt
-mv ./cooperation.txt $OUTDIR/cooperation1.txt
+./run_single
+# mv ./time.txt $OUTDIR/time1.txt
+# mv ./wealth.txt $OUTDIR/wealth1.txt
+# mv ./cooperation.txt $OUTDIR/cooperation1.txt
+mv ./time.txt time1.txt
+mv ./wealth.txt wealth1.txt
+mv ./cooperation.txt cooperation1.txt
 
 
 
 sed "s|Choice= 1|Choice= 2|" ./dummy.conf > ./config.conf
-./$(PROG)
-mv ./time.txt $OUTDIR/time2.txt
-mv ./wealth.txt $OUTDIR/wealth2.txt
-mv ./cooperation.txt $OUTDIR/cooperation2.txt
-
+./run_single
+mv ./time.txt time2.txt
+mv ./wealth.txt wealth2.txt
+mv ./cooperation.txt cooperation2.txt
 
 sed "s|Choice= 1|Choice= 3|" ./dummy.conf > ./config.conf
-./$(PROG)
-mv ./time.txt $OUTDIR/time3.txt
-mv ./wealth.txt $OUTDIR/wealth3.txt
-mv ./cooperation.txt $OUTDIR/cooperation3.txt
+./run_single
+mv ./time.txt time3.txt
+mv ./wealth.txt wealth3.txt
+mv ./cooperation.txt cooperation3.txt
 
 
 sed "s|Choice= 1|Choice= 4|" ./dummy.conf > ./config.conf
-./$(PROG)
-mv ./time.txt $OUTDIR/time4.txt
-mv ./wealth.txt $OUTDIR/wealth4.txt
-mv ./cooperation.txt $OUTDIR/cooperation4.txt
+./run_single
+mv ./time.txt time4.txt
+mv ./wealth.txt wealth4.txt
+mv ./cooperation.txt cooperation4.txt
 
-rm config.conf
-mv parameters.txt $OUTDIR/
+# rm config.conf
+# mv parameters.txt $OUTDIR/
